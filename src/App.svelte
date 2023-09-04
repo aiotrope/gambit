801d08c61460c1197d627276b59e1e63b6e9ed73
aiotrope <arneli74@protonmail.com>
2023-09-04 12:27:01 +0300

    🐛 fix(app): solutions for customize invitation letter and listing text exercise

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,5 @@
 <script>
-  let list = ['Never', 'gonna', 'give', 'you', 'up'];
-  let index = 0;
-  const maxIndex = list.length - 1
-
-  const updateIndex = () => {
-    if (index === maxIndex) {
-      index = 0
-    } else {
-      index++
-    }
-  };
+  import Monitor from "./lib/Monitor.svelte";
 </script>
 
-<h1>{list[index]}</h1>
-<p>Elements: {index}</p>
-<button on:click={updateIndex}>Next!</button>
+<Monitor />
---
+++ src/lib/Editor.svelte
@@ -0,0 +1,14 @@
+<script>
+  let text = '';
+  export let addText;
+
+  const textTyped = () => {
+    if (text.length === 0) return;
+
+    addText(text);
+    text = '';
+  };
+</script>
+
+<input type="text" bind:value={text} />
+<button on:click={textTyped}>Add text</button>
